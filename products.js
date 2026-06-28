ab7bbedfcf9da346464fa7922820ff7f7a395c4a
guitarworkshop <sorry258@gmail.com>
2026-06-29 02:22:36 +0800

    Delete products.js

--- products.js
+++
@@ -1,6 +0,0 @@
-const products = [
-  { brand:"DADARWOOD", model:"D20-GAC PLUS", desc:"桃花心木面單板", price:"NT$ 8,200" },
-  { brand:"DADARWOOD", model:"D30-GAC PLUS", desc:"雲杉面單板 / 玫瑰木側背", price:"NT$ 10,200" },
-  { brand:"ANISA", model:"AS-01", desc:"全相思木全單板", price:"歡迎洽詢" },
-  { brand:"EITMOSS", model:'EITMOSS 36"', desc:"旅行吉他", price:"歡迎洽詢" }
-];
